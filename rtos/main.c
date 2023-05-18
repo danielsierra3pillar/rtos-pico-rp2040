4a7da2562dae63b6ba99daccd3bfb55102e80a2f
Daniel Sierra <daniel.sierra@3pillarglobal.com>
2023-05-17 23:03:14 -0600

    A LOT of changes

---
+++ rtos/main.c
@@ -0,0 +1,158 @@
+#include <stdio.h>
+#include "pico/stdlib.h"
+#include "hardware/gpio.h"
+#include "hardware/timer.h"
+#include "pico/mutex.h"
+
+#define LED_PIN PICO_DEFAULT_LED_PIN
+
+typedef struct
+{
+    void (*thread_func)(void);
+    uint32_t priority;
+    uint32_t remaining_time;
+    uint32_t thread_id;
+    uint32_t waiting;
+} ThreadControlBlock;
+
+#define NUM_THREADS 2
+ThreadControlBlock thread_blocks[NUM_THREADS];
+volatile uint32_t current_thread;
+
+mutex_t led_mutex;
+
+void my_mutex_init(mutex_t *mutex)
+{
+    mutex_init(mutex);
+}
+
+void my_mutex_acquire(mutex_t *mutex)
+{
+    while (!try_mutex(mutex))
+    {
+        // Spin until the mutex becomes available
+        printf("Thread ID %d is waiting for the mutex.\n", thread_blocks[current_thread].thread_id);
+    }
+}
+
+void my_mutex_release(mutex_t *mutex)
+{
+    unlock_mutex(mutex);
+}
+
+void thread1_func(void)
+{
+    while (1)
+    {
+        my_mutex_acquire(&led_mutex);
+        printf("Thread ID %d acquired the mutex.\n", thread_blocks[current_thread].thread_id);
+        gpio_put(LED_PIN, 1);
+        sleep_ms(500);
+        gpio_put(LED_PIN, 0);
+        sleep_ms(500);
+        my_mutex_release(&led_mutex);
+        printf("Thread ID %d released the mutex.\n", thread_blocks[current_thread].thread_id);
+        yield(); // Context switch to the next thread
+    }
+}
+
+void thread2_func(void)
+{
+    while (1)
+    {
+        my_mutex_acquire(&led_mutex);
+        printf("Thread ID %d acquired the mutex.\n", thread_blocks[current_thread].thread_id);
+        gpio_put(LED_PIN, 1);
+        sleep_ms(200);
+        gpio_put(LED_PIN, 0);
+        sleep_ms(800);
+        my_mutex_release(&led_mutex);
+        printf("Thread ID %d released the mutex.\n", thread_blocks[current_thread].thread_id);
+        yield(); // Context switch to the next thread
+    }
+}
+
+void kernel_tick_handler(struct repeating_timer *t)
+{
+    static uint32_t tick = 0;
+    tick++;
+
+    for (int i = 0; i < NUM_THREADS; i++)
+    {
+        if (i != current_thread)
+        {
+            if (tick % thread_blocks[i].priority == 0)
+                thread_blocks[i].remaining_time = 0;
+        }
+    }
+}
+
+void kernel_thread_scheduler()
+{
+    uint32_t highest_priority = 0;
+    uint32_t next_thread = current_thread;
+
+    for (int i = 0; i < NUM_THREADS; i++)
+    {
+        if (i != current_thread)
+        {
+            if (thread_blocks[i].priority > highest_priority && thread_blocks[i].remaining_time == 0)
+            {
+                highest_priority = thread_blocks[i].priority;
+                next_thread = i;
+            }
+        }
+    }
+
+    if (next_thread != current_thread)
+    {
+        current_thread = next_thread;
+        printf("Context Switch: Thread ID %d | Priority %d\n", thread_blocks[current_thread].thread_id, thread_blocks[current_thread].priority);
+        thread_blocks[current_thread].thread_func();
+        thread_blocks[current_thread].remaining_time = thread_blocks[current_thread].priority;
+    }
+}
+
+void yield()
+{
+    uint32_t next_thread = (current_thread + 1) % NUM_THREADS;
+    current_thread = next_thread;
+
+    printf("Context Switch: Thread ID %d | Priority %d\n", thread_blocks[current_thread].thread_id, thread_blocks[current_thread].priority);
+    thread_blocks[current_thread].remaining_time = thread_blocks[current_thread].priority;
+}
+
+int main()
+{
+    stdio_init_all();
+    gpio_init(LED_PIN);
+    gpio_set_dir(LED_PIN, GPIO_OUT);
+
+    my_mutex_init(&led_mutex);
+
+    thread_blocks[0].thread_func = thread1_func;
+    thread_blocks[0].priority = 1000;
+    thread_blocks[0].remaining_time = 0;
+    thread_blocks[0].thread_id = 1;
+    thread_blocks[0].waiting = 0;
+
+    thread_blocks[1].thread_func = thread2_func;
+    thread_blocks[1].priority = 2000;
+    thread_blocks[1].remaining_time = 0;
+    thread_blocks[1].thread_id = 2;
+    thread_blocks[1].waiting = 0;
+
+    current_thread = 0;
+
+    uint32_t interval_us = 1000; // 1ms
+    struct repeating_timer timer;
+    add_repeating_timer_us(-interval_us, kernel_tick_handler, NULL, &timer);
+
+    while (1)
+    {
+        printf("Thread ID %d | Priority %d\n", thread_blocks[current_thread].thread_id, thread_blocks[current_thread].priority);
+        kernel_thread_scheduler();
+    }
+
+    return 0;
+}
--- tasks-and-queues/main.c
+++ tasks-and-queues/main.c
@@ -1,63 +1,117 @@
-
-#include <stdio.h>
 #include "pico/stdlib.h"
-#include "FreeRTOS.h"
-#include "task.h"
-#include "queue.h"
+#include "hardware/gpio.h"
 
-static QueueHandle_t xQueue = NULL;
+#define LED_PIN PICO_DEFAULT_LED_PIN
 
-void led_task(void *pvParameters)
+typedef struct
 {
-    const uint LED_PIN = PICO_DEFAULT_LED_PIN;
-    uint uIValueToSend = 0;
-    gpio_init(LED_PIN);
-    gpio_set_dir(LED_PIN, GPIO_OUT);
+    void (*thread_func)(void);
+    uint32_t priority;
+    uint32_t remaining_time;
+    uint32_t thread_id;
+} ThreadControlBlock;
 
-    while (true)
+#define NUM_THREADS 2
+ThreadControlBlock thread_blocks[NUM_THREADS];
+volatile uint32_t current_thread;
+
+void thread1_func(void)
+{
+    while (1)
     {
         gpio_put(LED_PIN, 1);
-        uIValueToSend = 1;
-        xQueueSend(xQueue, &uIValueToSend, 0U);
-        vTaskDelay(100);
-
+        sleep_ms(500);
         gpio_put(LED_PIN, 0);
-        uIValueToSend = 0;
-        xQueueSend(xQueue, &uIValueToSend, 0U);
-        vTaskDelay(100);
+        sleep_ms(500);
+        yield(); // Context switch to the next thread
     }
 }
 
-void usb_task(void *pvParameters)
+void thread2_func(void)
 {
-    uint uIReceivedValue;
-
     while (1)
     {
-        xQueueReceive(xQueue, &uIReceivedValue, portMAX_DELAY);
+        gpio_put(LED_PIN, 1);
+        sleep_ms(200);
+        gpio_put(LED_PIN, 0);
+        sleep_ms(800);
+        yield(); // Context switch to the next thread
+    }
+}
 
-        if (uIReceivedValue == 1)
+void kernel_tick_handler()
+{
+    static uint32_t tick = 0;
+    tick++;
+
+    for (int i = 0; i < NUM_THREADS; i++)
+    {
+        if (i != current_thread)
         {
-            printf("LED is ON! \n");
-        }
-        if (uIReceivedValue == 0)
-        {
-            printf("LED is OFF! \n");
+            if (tick % thread_blocks[i].priority == 0)
+                thread_blocks[i].remaining_time = 0;
         }
     }
+}
+
+void kernel_thread_scheduler()
+{
+    uint32_t highest_priority = 0;
+    uint32_t next_thread = current_thread;
+
+    for (int i = 0; i < NUM_THREADS; i++)
+    {
+        if (i != current_thread)
+        {
+            if (thread_blocks[i].priority > highest_priority && thread_blocks[i].remaining_time == 0)
+            {
+                highest_priority = thread_blocks[i].priority;
+                next_thread = i;
+            }
+        }
+    }
+
+    if (next_thread != current_thread)
+    {
+        current_thread = next_thread;
+        printf("Context Switch: Thread ID %d | Priority %d\n", thread_blocks[current_thread].thread_id, thread_blocks[current_thread].priority);
+        thread_blocks[current_thread].thread_func();
+        thread_blocks[current_thread].remaining_time = thread_blocks[current_thread].priority;
+    }
+}
+
+void yield()
+{
+    uint32_t next_thread = (current_thread + 1) % NUM_THREADS;
+    current_thread = next_thread;
+
+    printf("Context Switch: Thread ID %d | Priority %d\n", thread_blocks[current_thread].thread_id, thread_blocks[current_thread].priority);
+    thread_blocks[current_thread].remaining_time = thread_blocks[current_thread].priority;
 }
 
 int main()
 {
     stdio_init_all();
+    gpio_init(LED_PIN);
+    gpio_set_dir(LED_PIN, GPIO_OUT);
 
-    xQueue = xQueueCreate(1, sizeof(uint));
+    thread_blocks[0].thread_func = thread1_func;
+    thread_blocks[0].priority = 1000;
+    thread_blocks[0].remaining_time = 0;
+    thread_blocks[0].thread_id = 1;
 
-    xTaskCreate(led_task, "LED_Task", 256, NULL, 1, NULL);
-    xTaskCreate(usb_task, "USB_Task", 256, NULL, 1, NULL);
-    vTaskStartScheduler();
+    thread_blocks[1].thread_func = thread2_func;
+    thread_blocks[1].priority = 2000;
+    thread_blocks[1].remaining_time = 0;
+    thread_blocks[1].thread_id = 2;
+
+    current_thread = 0;
 
     while (1)
     {
-    };
+        printf("Thread ID %d | Priority %d\n", thread_blocks[current_thread].thread_id, thread_blocks[current_thread].priority);
+        kernel_thread_scheduler();
+    }
+
+    return 0;
 }
